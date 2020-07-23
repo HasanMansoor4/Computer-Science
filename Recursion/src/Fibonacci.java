30981bb42ddc08b70d9cdc427ebfc3a3ad60b579
Hasan Mansoor <hassan72702@gmail.com>
2020-07-23 12:07:42 +0300

    Implement Recursion/Fibonacci.java

---
+++ Recursion/src/Fibonacci.java
@@ -0,0 +1,46 @@
+// Starts with 0 1 -> Sum of two previous numbers -> next number
+// 0 1 1 2 3 5 8 13 21 34 55 89 144 
+//  Fn = Fn-1 +  Fn-2
+
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		System.out.println(recFib(10));
+		System.out.println(iterFib(10));
+	}
+	
+	
+	// Recursive implementation 
+	static int recFib(int n) {
+		if (n == 1 || n == 0) {
+			return n;
+		}
+		
+		return recFib(n-1) + recFib(n-2);
+	}
+	
+	// Iterative implementation 
+	static int iterFib(int n) {
+		
+		if (n == 0 || n == 1) {
+			return n; 
+		} 
+		
+		int first = 0;
+		int second = 1;
+		int nth = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			nth = first + second;
+			first = second;
+			second = nth;
+		} 
+		
+		return nth;
+
+	}
+	
+	
+
+}
