b24ed8f12a136109d92706667ba3dbd2972bcef6
Hasan Mansoor <hassan72702@gmail.com>
2021-03-27 17:21:21 +0300

    Java Fib

--- Recursion/Fibonacci/src/Fibonacci.java
+++ Recursion/Fibonacci/src/Fibonacci.java
@@ -31,7 +31,7 @@ fib(4)               	 fib(3)
 /        \              /       \ 
 fib(3)      fib(2)       fib(2)   fib(1)
 /    \       /    \        /      \
-fib(2)   fib(1)  fib(1) fib(0) fib(1) fib(0)
+fib(2)   fib(1)  fib(0) fib(0) fib(1) fib(0)
 /     \
 fib(1) fib(0)
 
